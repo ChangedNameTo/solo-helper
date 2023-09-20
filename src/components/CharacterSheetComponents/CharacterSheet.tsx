63d257fa199aed399ad99bdfd076e95de4d9c760
Will Mitchell <will.n.mitchell@gmail.com>
2023-09-19 23:43:45 -0400

    Basic character form working

--- src/components/CharacterSheetComponents/CharacterSheet.tsx
+++ src/components/CharacterSheetComponents/CharacterSheet.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { PencilIcon } from "@heroicons/react/20/solid";
 
 import { testCharacter } from "../../Characters/IronswornCharacter";
 import SheetGauge from "./SheetGauge";
@@ -11,7 +12,7 @@ import SheetBurden from "./SheetBurden";
 import SheetCompanion from "./SheetCompanion";
 import SheetAsset from "./SheetAsset";
 import SheetExperienceBar from "./SheetExperienceBar";
-import { PencilIcon } from "@heroicons/react/20/solid";
+import CharacterForm from "../CharacterForm";
 
 export default function CharacterSheet() {
   const characterSheetRef = React.useRef({} as HTMLDivElement);
@@ -42,164 +43,184 @@ export default function CharacterSheet() {
     };
   }, []);
 
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const closeModal = (): void => {
+    setIsOpen(false);
+  };
+
+  const openModal = (): void => {
+    setIsOpen(true);
+  };
+
   return (
-    <div className="overflow-scroll bg-gray-100" ref={characterSheetRef}>
-      {/* Name */}
-      <div className="my-2 mx-2 flex flex-row shadow-sm rounded-md bg-white">
-        <div className="pl-2 text-xl font-semibold text-center">
-          {testCharacter.name}
+    <>
+      <div className="overflow-scroll bg-gray-100" ref={characterSheetRef}>
+        {/* Name */}
+        <div className="my-2 mx-2 flex flex-row shadow-sm rounded-md bg-white">
+          <div className="flex-grow pl-2 text-xl font-semibold text-center">
+            {testCharacter.name}
+          </div>
+          <button
+            className="place-content-end flex-shrink bg-indigo-600 hover:bg-indigo-500 rounded-lg px-2 py-1 my-1 mx-2"
+            onClick={openModal}
+          >
+            <PencilIcon className="h-5 w-5 text-white" aria-hidden="true" />
+          </button>
         </div>
-        <button className="justify-right flex-shrink bg-indigo-600 hover:bg-indigo-500 rounded-lg px-2 py-1 my-1">
-          <PencilIcon className="h-5 w-5 text-white" aria-hidden="true" />
-        </button>
-      </div>
 
-      <div className="my-2 mx-2 flex flex-col shadow-sm rounded-md bg-white">
-        <div className="text-l font-semibold text-center">Experience</div>
-        <SheetExperienceBar experience={testCharacter.experience} />
-      </div>
-
-      {/* Stats */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm m-2 border-gray-200 border">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h2 className="text-lg font-semibold leading-6 text-gray-900">
-            Stats
-          </h2>
+        <div className="my-2 mx-2 flex flex-col shadow-sm rounded-md bg-white">
+          <div className="text-l font-semibold text-center">Experience</div>
+          <SheetExperienceBar experience={testCharacter.experience} />
         </div>
-        <ul role="list" className="flex flex-col">
-          {testCharacter.stats.map((stat, idx, arr) => (
-            <SheetStat stat={stat} idx={idx} key={stat.name} arr={arr} />
+
+        {/* Stats */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm m-2 border-gray-200 border">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h2 className="text-lg font-semibold leading-6 text-gray-900">
+              Stats
+            </h2>
+          </div>
+          <ul role="list" className="flex flex-col">
+            {testCharacter.stats.map((stat, idx, arr) => (
+              <SheetStat stat={stat} idx={idx} key={stat.name} arr={arr} />
+            ))}
+          </ul>
+        </div>
+
+        {/* Gauges */}
+        <div className="text-l font-semibold text-center">Gauges</div>
+        <ul role="list" className="mx-2 flex flex-col gap-2">
+          {testCharacter.gauges.map((gauge, idx) => (
+            <SheetGauge gauge={gauge} key={gauge.name} />
           ))}
         </ul>
-      </div>
 
-      {/* Gauges */}
-      <div className="text-l font-semibold text-center">Gauges</div>
-      <ul role="list" className="mx-2 flex flex-col gap-2">
-        {testCharacter.gauges.map((gauge, idx) => (
-          <SheetGauge gauge={gauge} key={gauge.name} />
-        ))}
-      </ul>
-
-      {/* Vows */}
-      <div className="text-l font-semibold text-center">Vows</div>
-      <ul role="list" className="mx-2 flex flex-col gap-2">
-        {testCharacter.vows.map((vow) => (
-          <SheetVow vow={vow} key={vow.name} />
-        ))}
-      </ul>
-
-      {/* Bonds */}
-      <div className="text-l font-semibold text-center">Bonds</div>
-      <ul role="list" className="mx-2 flex flex-col gap-2">
-        {testCharacter.bonds.bonds.map((bond) => (
-          <SheetBond bond={bond} key={bond.name} />
-        ))}
-      </ul>
-
-      {/* Debilities */}
-      <div className="text-l font-semibold text-center">Debilities</div>
-
-      {/* Conditions */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Conditions
-          </h3>
-        </div>
-        <ul role="list" className="divide-y divide-gray-200">
-          {testCharacter.conditions.map((condition) => (
-            <SheetCondition condition={condition} key={condition.name} />
+        {/* Vows */}
+        <div className="text-l font-semibold text-center">Vows</div>
+        <ul role="list" className="mx-2 flex flex-col gap-2">
+          {testCharacter.vows.map((vow) => (
+            <SheetVow vow={vow} key={vow.name} />
           ))}
         </ul>
-      </div>
 
-      {/* Banes */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Banes
-          </h3>
-        </div>
-        <ul role="list" className="divide-y divide-gray-200">
-          {testCharacter.banes.map((bane) => (
-            <SheetBane bane={bane} key={bane.name} />
+        {/* Bonds */}
+        <div className="text-l font-semibold text-center">Bonds</div>
+        <ul role="list" className="mx-2 flex flex-col gap-2">
+          {testCharacter.bonds.bonds.map((bond) => (
+            <SheetBond bond={bond} key={bond.name} />
           ))}
         </ul>
-      </div>
 
-      {/* Burdens */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Burdens
-          </h3>
+        {/* Debilities */}
+        <div className="text-l font-semibold text-center">Debilities</div>
+
+        {/* Conditions */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              Conditions
+            </h3>
+          </div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {testCharacter.conditions.map((condition) => (
+              <SheetCondition condition={condition} key={condition.name} />
+            ))}
+          </ul>
         </div>
-        <ul role="list" className="divide-y divide-gray-200">
-          {testCharacter.burdens.map((burden) => (
-            <SheetBurden burden={burden} key={burden.name} />
-          ))}
-        </ul>
-      </div>
 
-      {/* Assets */}
-      <div className="text-l font-semibold text-center">Assets</div>
-
-      {/* Companions */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Companions
-          </h3>
+        {/* Banes */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              Banes
+            </h3>
+          </div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {testCharacter.banes.map((bane) => (
+              <SheetBane bane={bane} key={bane.name} />
+            ))}
+          </ul>
         </div>
-        <ul role="list" className="divide-y divide-gray-200">
-          {testCharacter.companions.map((companion) => (
-            <SheetCompanion companion={companion} key={companion.name} />
-          ))}
-        </ul>
-      </div>
 
-      {/* Paths */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Paths
-          </h3>
+        {/* Burdens */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              Burdens
+            </h3>
+          </div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {testCharacter.burdens.map((burden) => (
+              <SheetBurden burden={burden} key={burden.name} />
+            ))}
+          </ul>
         </div>
-        <ul role="list" className="divide-y divide-gray-200">
-          {testCharacter.paths.map((path) => (
-            <SheetAsset asset={path} key={path.name} />
-          ))}
-        </ul>
-      </div>
 
-      {/* Talents */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Talents
-          </h3>
-        </div>
-        <ul role="list" className="divide-y divide-gray-200">
-          {testCharacter.talents.map((talent) => (
-            <SheetAsset asset={talent} key={talent.name} />
-          ))}
-        </ul>
-      </div>
+        {/* Assets */}
+        <div className="text-l font-semibold text-center">Assets</div>
 
-      {/* Rituals */}
-      <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
-        <div className="border-b border-gray-200 bg-white px-4 py-2">
-          <h3 className="text-base font-semibold leading-6 text-gray-900">
-            Rituals
-          </h3>
+        {/* Companions */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              Companions
+            </h3>
+          </div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {testCharacter.companions.map((companion) => (
+              <SheetCompanion companion={companion} key={companion.name} />
+            ))}
+          </ul>
         </div>
-        <ul role="list" className="divide-y divide-gray-200">
-          {testCharacter.rituals.map((ritual) => (
-            <SheetAsset asset={ritual} key={ritual.name} />
-          ))}
-        </ul>
+
+        {/* Paths */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              Paths
+            </h3>
+          </div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {testCharacter.paths.map((path) => (
+              <SheetAsset asset={path} key={path.name} />
+            ))}
+          </ul>
+        </div>
+
+        {/* Talents */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              Talents
+            </h3>
+          </div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {testCharacter.talents.map((talent) => (
+              <SheetAsset asset={talent} key={talent.name} />
+            ))}
+          </ul>
+        </div>
+
+        {/* Rituals */}
+        <div className="overflow-hidden bg-white rounded-md shadow-sm mx-2 my-2">
+          <div className="border-b border-gray-200 bg-white px-4 py-2">
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              Rituals
+            </h3>
+          </div>
+          <ul role="list" className="divide-y divide-gray-200">
+            {testCharacter.rituals.map((ritual) => (
+              <SheetAsset asset={ritual} key={ritual.name} />
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+      <CharacterForm
+        isOpen={isOpen}
+        closeModal={closeModal}
+        character={testCharacter}
+      />
+    </>
   );
 }
--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
 import { Fragment, useState, useContext } from "react";
-import { Dialog, Transition } from "@headlessui/react";
 
 import Header from "./Header";
 import { GamesContext, GamesDispatchContext } from "../Contexts/GamesContext";
 import GameButton from "./GameButton";
 import CharacterForm from "./CharacterForm";
-import { blankCharacter } from "../Characters/DefaultCharacter";
 
 export default function GamesList() {
   const games = useContext(GamesContext);
@@ -26,7 +24,7 @@ export default function GamesList() {
     <>
       <div className="flex flex-col h-screen grow">
         <Header />
-        <div className="flex-1 mt-2">
+        <div className="flex-1 pt-2">
           <div className="mx-auto max-w-7xl px-4">
             <div className="flex justify-between items-center mb-4">
               <h1 className="text-3xl font-bold text-gray-800 tracking-tight">
@@ -50,45 +48,7 @@ export default function GamesList() {
         </div>
       </div>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-bold leading-6 text-gray-900"
-                  >
-                    New Character Creation
-                  </Dialog.Title>
-                  <CharacterForm character={blankCharacter} />
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
+      <CharacterForm isOpen={isOpen} closeModal={closeModal} />
     </>
   );
 }
